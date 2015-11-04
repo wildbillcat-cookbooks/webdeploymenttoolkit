149faaead53763696067b4ac9f3a48aa5c5768ae
Patrick McMorran <kingrolloinc@sbcglobal.net>
2015-11-03 19:56:04 -0500

    Changed how resource was defined

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'kingrolloinc@sbcglobal.net'
 license 'The Unlicense'
 description 'Installs/Configures webdeploymenttoolkit'
 long_description 'Installs/Configures webdeploymenttoolkit'
-version '0.1.2'
+version '0.1.3'
 issues_url "https://github.com/wildbillcat-cookbooks/webdeploymenttoolkit/issues"
 source_url "https://github.com/wildbillcat-cookbooks/webdeploymenttoolkit"
 
--- resources/web_deploy.rb
+++ resources/web_deploy.rb
@@ -1,10 +1,6 @@
 actions :sync, :delete, :dump, :getDependencies, :getSystemInfo
+default_action :sync
 
 attribute :source, :kind_of => String, :name_attribute => true
 attribute :dest, :kind_of => String
 attribute :parameters, :kind_of => Hash
-
-def initialize(name, run_context=nil)
-  super
-  @action = :sync
-end
